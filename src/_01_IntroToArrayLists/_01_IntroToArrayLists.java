c1b8ca49d758c4104cf8f3f7cfa0e33481fd2200
League <league@WTS9.attlocal.net>
2018-09-16 15:56:20 -0700

    Level 3 Mod. 1 DIA and IntroToArrayLists (In Progress)
    9/16/18
    
    Finished Dynamic Integer Array, although i'm not sure if the test is broken or literally -all- of my code is nonfunctional... Regardless, I did what the recipe said and moved on.
    I also worked on IntroToArrayLists, which I got to about Step 3-ish.  Probably need to get help on that, but otherwise a good class.

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -5,15 +5,24 @@ import java.util.ArrayList;
 public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> str = new ArrayList<String>();
 		//   Don't forget to import the ArrayList class
 
 		//2. Add five Strings to your list
-		
+		str.add("a");
+		str.add("b");
+		str.add("c");
+		str.add("d");
+		str.add("e");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < str.size(); i++) {
+			System.out.println("String at element " + i + " is " + str.get(i));
+		}
 		
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s : str) {
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
 		
 		//6. Print all the Strings in reverse order.
