5a34cfbae46987fd25b101995f0be560fc19a797
SunStar56 <league@WTS22.attlocal.net>
2018-11-11 15:54:14 -0800

    Level 3 Mod. 2 TextUndoRedo
    11/11/18
    Still working on the text editor.

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -8,23 +8,27 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo implements KeyListener{
-		JFrame frame;
-		JPanel panel;
-		JLabel label;
-		Stack<Character> chars;
-		
+public class _02_TextUndoRedo implements KeyListener {
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Stack<Character> chars;
+	Stack<Character> deletedChars;
+	String CharString;
+
 	public _02_TextUndoRedo() {
 		chars = new Stack<Character>();
-		createUI(chars);	
-	
+		deletedChars = new Stack<Character>();
+		createUI(chars);
+
 	}
-	
+
 	public static void main(String[] args) {
 		new _02_TextUndoRedo();
-		
-		}
-	public void createUI(Stack<Character> chars){
+
+	}
+
+	public void createUI(Stack<Character> chars) {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		label = new JLabel("");
@@ -34,44 +38,51 @@ public class _02_TextUndoRedo implements KeyListener{
 		frame.pack();
 
 	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
-	
-		
-		
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {			
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			deletedChars.push(chars.peek());
+			System.out.println(deletedChars);
 			chars.pop();
-			label.setText(label.getText());
-			label.setText(label.getText() + "");
-			System.out.println("done");
-		}
-		else {
+			System.out.println(chars);
+			for (int i = 0; i < chars.size() - 1; i++) {
+				System.out.println(chars.get(i));
+				CharString = CharString + Character.toString(chars.get(i));
+			}
+			label.setText(CharString);
+		} else {
 			label.setText(label.getText() + e.getKeyChar() + "");
 			frame.pack();
 			Character c = e.getKeyChar();
 			chars.push(c);
 		}
-		
+
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 	}
-	
+
 }
-	/* 
-	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
-	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
-	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
-	 * 
-	 */
+/*
+ * Create a JFrame with a JPanel and a JLabel.
+ * 
+ * Every time a key is pressed, add that character to the JLabel. It should look
+ * like a basic text editor.
+ * 
+ * Make it so that every time the BACKSPACE key is pressed, the last character
+ * is erased from the JLabel. Save that deleted character onto a Stack of
+ * Characters.
+ * 
+ * Choose a key to be the Undo key. Make it so that when that key is pressed,
+ * the top Character is popped off the Stack and added back to the JLabel.
+ * 
+ */
